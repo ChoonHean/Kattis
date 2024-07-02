968db3af4959514de923fe0ede45f68910d9e1aa
ChoonHean <e0958179@u.nus.edu>
2024-07-02 15:24:37 +0800

    Add questions

--- main.cpp
+++ main.cpp
@@ -36,59 +36,6 @@ inline void print_p(T v) {
 }
 
 inline void solve() {
-    string s;
-    getline(cin,s);
-    unordered_map<char,pii>map;
-    vector<vc>grid(5,vc(5));
-    int row=0,col=0;
-    for(char c:s){
-        if(c=='q'||c==' ')continue;
-        if(map.contains(c))continue;
-        map[c]=pair(row,col);
-        grid[row][col]=toupper(c);
-        col++;
-        if(col==5){
-            col=0;
-            row++;
-        }
-    }
-    for(char c='a';c<='z';c++){
-        if(c=='q')continue;
-        if(map.contains(c))continue;
-        map[c]=pair(row,col);
-        grid[row][col]=toupper(c);
-        col++;
-        if(col==5){
-            col=0;
-            row++;
-        }
-    }
-    string res;
-    getline(cin,s);
-    for(int i=0;;){
-        if(s[i]==' ')s.erase(i,1);
-        else i++;
-        if(i>=s.size())break;
-    }
-    char a,b;
-    for(int i=0;i<s.size();i++){
-        a=s[i];
-        if(i==s.size()-1)b='x';
-        else if(s[i]==s[i+1])b='x';
-        else b=s[++i];
-        pii x=map[a],y=map[b];
-        if(x.first==y.first){
-            res+=grid[x.first][(x.second+1)%5];
-            res+=grid[y.first][(y.second+1)%5];
-        }else if(x.second==y.second){
-            res+=grid[(x.first+1)%5][x.second];
-            res+=grid[(y.first+1)%5][y.second];
-        }else{
-            res+=grid[x.first][y.second];
-            res+=grid[y.first][x.second];
-        }
-    }
-    cout<<res;
 }
 
 int main() {
