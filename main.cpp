bf7a3a97d98590771514ff2ff41fe5bb0a265715
ChoonHean <e0958179@u.nus.edu>
2024-08-09 00:01:06 +0800

    Add questions

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ typedef vector<ll> vl;
 typedef pair<int, int> pii;
 typedef pair<double, double> pdd;
 typedef pair<ll, ll> pll;
-const ll inf = 1e15;
+const int inf = 1e9;
 const int mod = 1e9 + 7;
 #define all(a) a.begin(),a.end()
 #define read(n) vi arr(n);for(int i=0;i<n;i++)cin>>arr[i]
@@ -95,65 +95,26 @@ inline void p(stack<T> s) {
 }
 
 inline void solve() {
-    int n, m, u, v, t;
-    cin >> n >> m;
-    vector<vector<pii>> adj(n);
-    rep(0, m) {
-        cin >> u >> v >> t;
-        u--;
-        v--;
-        adj[u].pb(pair(v, t));
-        adj[v].pb(pair(u, t));
+    int money,aim,d,p;
+    string s;
+    cin>>s;
+    s.erase(s.find('.'));
+    money=stoi(s)*100;
+    cin>>s>>d;
+    s.erase(s.find('.'));
+    aim=stoi(s)*10*6;
+    vs names(d);
+    vector<pii>arr(d);
+    rep(0,d){
+        cin>>names[i]>>p>>s;
+        if(s[2]=='1')arr[i].first=p*6;
+        else if(s[2]=='2')arr[i].first=p*3;
+        else arr[i].first=p*2;
+        cin>>s;
+        s.erase(s.find('.'));
+        arr[i].second=stoi(s)*100;
     }
-    vi loc(7);
-    rep(0, 7) {
-        cin >> loc[i];
-        loc[i]--;
-    }
-    vector<vl> dist(7, vl(n, inf));
-    rep(0, 7) {
-        dist[i][loc[i]] = 0;
-        set<pll> tree;
-        for (int j = 0; j < n; j++)tree.emplace_hint(tree.end(), inf, j);
-        tree.erase(pair(inf, loc[i]));
-        for (auto [to, w]: adj[loc[i]]) {
-            tree.erase(pair(inf, to));
-            tree.emplace(w, to);
-            dist[i][to] = w;
-        }
-        while (!tree.empty()) {
-            pll curr = *tree.begin();
-            if (curr.first == inf)break;
-            tree.erase(tree.begin());
-            for (auto [to, w]: adj[curr.second]) {
-                ll next = curr.first + w;
-                if (next < dist[i][to]) {
-                    tree.erase(pair(dist[i][to], to));
-                    tree.emplace(pair(next, to));
-                    dist[i][to] = next;
-                }
-            }
-        }
-    }
-    vector<vl> dp(n, vl(1 << 7, inf));
-    dp[0][0] = 0;
-    for (int shift = 0; shift < 7; shift++) {
-        dp[loc[shift]][1 << shift] = dist[shift][0];
-    }
-    rep(1, 1 << 7) {
-        for (int j: loc) {
-            for (int shift = 0; shift < 7; shift++) {
-                if (!((i >> shift) & 1)) {
-                    int next = loc[shift];
-                    int state = i | (1 << shift);
-                    dp[next][state] = min(dp[next][state], dp[j][i] + dist[shift][j]);
-                }
-            }
-        }
-    }
-    ll res = inf;
-    for (int i: loc)res = min(res, dp[i][(1 << 7) - 1]);
-    res == inf ? p(-1) : p(res);
+    for(auto a:arr)cout<<a.first<<' '<<a.second<<nl;
 }
 
 int main() {
